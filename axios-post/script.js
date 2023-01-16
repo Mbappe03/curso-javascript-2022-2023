f3b42f63312517bd268f535de4530fe10582f58a
Cesar Daal <cesardaalch@gmail.com>
2023-01-16 11:43:21 -0430

    curso-javascript-2022-2023

---
+++ axios-post/script.js
@@ -0,0 +1,13 @@
+const button = document.getElementById("button")
+button.addEventListener("click",() => {
+    axios({
+        method:"POST",
+        url:"https://jsonplaceholder.typicode.com/post",
+        data:{
+            title:"A new post",
+            body:"Lorem ipsum dolor sit amet consectetur adipisicing elit",
+            userId:1
+        }
+    }).then(res => console.log(res.data))
+    .catch(err => console.log(err))
+})
